bda6746653abe31866ed6f22e4943d07dea8ba54
Aniket-as <sirsataniket.as@gmail.com>
2026-06-20 14:40:54 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -46,7 +46,7 @@ const Login = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:8080/api/auth/login", {
+      const response = await fetch(""https://bhoomidarpan-5.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ aadhaarNumber, password }),
