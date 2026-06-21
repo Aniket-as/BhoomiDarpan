ff893e19bc71a2c052c71342b88b6685942325f0
Aniket-as <sirsataniket.as@gmail.com>
2026-06-21 19:01:59 +0530

    Update CORS policy to allow production origin

--- backend/src/main/java/com/bhoomidarpan/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/bhoomidarpan/security/JwtAuthenticationFilter.java
@@ -48,7 +48,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 
         // 🔥 ADD THIS BLOCK AT TOP
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+        response.setHeader("Access-Control-Allow-Origin", "https://bhoomi-darpan.vercel.app");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
